b797af74a3ea244d0d5943f3012c941842abe65b
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-07-09 22:51:49 -0600

    Resuelto conflicto en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './ui/pages/login/login.component';
 import { HomeComponent } from './ui/pages/home/home.component';
 import { PanelComponent } from './ui/pages/panel/panel.component';
 import { MedicalRecordsComponent } from './ui/pages/medical-records/medical-records.component';
+import { PatientHistoryComponent } from './ui/pages/patient-history/patient-history.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'panel', component: PanelComponent },
   { path: 'records', component: MedicalRecordsComponent },
+  {path: 'history', component: PatientHistoryComponent },
   
   {
     path: '',
