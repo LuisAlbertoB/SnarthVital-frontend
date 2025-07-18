9c14df051343185c394fef1aca1e177b5680140d
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-07-18 12:29:25 -0600

    historyPantient & login

--- src/app/ui/pages/patient-history/patient-history.component.ts
+++ src/app/ui/pages/patient-history/patient-history.component.ts
@@ -1,22 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+import { UserService } from '../../../features/user/user.service';
+import { User} from '../../../features/user/models/user';
 
 @Component({
   selector: 'app-patient-history',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, NavbarComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NavbarComponent,
+    DropdownModule,
+    CalendarModule,
+    ButtonModule,
+  ],
   templateUrl: './patient-history.component.html',
   styleUrls: ['./patient-history.component.css']
 })
-export class PatientHistoryComponent {
+export class PatientHistoryComponent implements OnInit {
   searchTerm = '';
-  patients = [
-    { name: 'Carlos Daniel Viencia Díaz' },
-    { name: 'Maximiliano Díaz Clemente' },
-    { name: 'Leonardo Daniel Velasco Gómez' },
-    { name: 'Joaquín Gómez Molina' },
+
+  patients: User[] = [];
+
+  // Dropdowns
+  selectedState: any;
+  stateOptions = [
+    { label: 'Todos los estados', value: null },
+    { label: 'Activo', value: 'active' },
+    { label: 'Crítico', value: 'critical' },
+    { label: 'Alta', value: 'discharged' },
   ];
+
+  selectedStation: any;
+  stationOptions = [
+    { label: 'Todas las estaciones', value: null },
+    { label: 'Estación 1', value: 'station1' },
+    { label: 'Estación 2', value: 'station2' },
+  ];
+
+  // Fechas
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    const doctorId = Number(localStorage.getItem('doctor_id'));
+    if (doctorId) {
+      this.userService.getPatients(doctorId).subscribe({
+        next: (data) => {
+          this.patients = data;
+        },
+        error: (error) => {
+          console.error('Error al obtener pacientes:', error);
+        }
+      });
+    } else {
+      console.warn('No se encontró doctor_id en localStorage.');
+    }
+  }
 }
